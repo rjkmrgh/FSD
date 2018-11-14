7f4d414188d3b53a6a7435f171fbb8a7bc7116d2
rajkumar <rajgoo12@gmail.com>
2018-11-14 10:23:02 +0530

    Task MAnager service

---
+++ TaskManagerService/taskmanagerdataaccess/ViewModel/TaskDetailsModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TaskManagerService.ViewModel
+{
+    public class TaskDetailsModel
+    {
+        [DataMember]
+        public int TaskId { get; set; } = 0;
+        [DataMember]
+        public string TaskName { get; set; }
+        [DataMember]
+        public int ParentTaskId { get; set; } = 0;
+        [DataMember]
+        public string ParentTaskName { get; set; }
+        [DataMember]
+        public int Priority { get; set; } = 0;
+        [DataMember]
+        public int PriorityTo { get; set; } = 0;
+        [DataMember]
+        public DateTime StartDate { get; set; }
+        [DataMember]
+        public DateTime EndDate { get; set; }
+        [DataMember]//(IsRequired = false)
+        public bool IsEnded { get; set; } = false;
+    }
+}
---
+++ TaskManagerService/taskmanagerservice/Controllers/TaskManagerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskManagerService.ViewModel;
+using TaskManagerDataAccess;
+
+namespace TaskManagerService.Controllers
+{
+    [RoutePrefix("api/taskmanager")]
+    public class TaskManagerController : ApiController
+    {
+        private TaskManagerDataAccess.TaskManagerDataAccess _taskManagerDataAccess = null;
+        public TaskManagerController()
+        {
+            _taskManagerDataAccess = new TaskManagerDataAccess.TaskManagerDataAccess();
+        }
+
+        [HttpPost]
+        [Route("addTask")]
+        public IHttpActionResult AddTask(TaskDetailsModel task)
+        {
+            _taskManagerDataAccess.AddTask(task);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("updateTask")]
+        public IHttpActionResult UpdateTask(TaskDetailsModel task)
+        {
+            _taskManagerDataAccess.UpdateTask(task);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("viewTask")]
+        public IHttpActionResult ViewTask(TaskDetailsModel task)
+        {
+            var result = _taskManagerDataAccess.ViewTask(task);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("endTask/{taskId}")]
+        public IHttpActionResult EndTask(int taskId)
+        {
+            _taskManagerDataAccess.EndTask(taskId);
+            return Ok();
+        }
+
+    }
+
+}
